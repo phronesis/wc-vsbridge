ff3c77bf3021c513fd35d607982f1dff2c5efae1
David Umoh <david.umoh@gloo.ng>
2018-10-04 15:50:25 +0100

    initial commit

---
+++ woocommerce-vsbridge.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WooCoomerce Vue Storefront Bridge
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     This plugin enables the connection of Vue Storefront - The first Ecommerce PWA - to WooCommerce
+ * Author:          David Umoh
+ * Author URI:      https://davidumoh.com
+ * Text Domain:     woocommerce-vsbridge
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Woocommerce_Vsbridge
+ */
+
+// Your code starts here.
